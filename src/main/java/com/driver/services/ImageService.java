66c1b8b3dac9105ec3eeaeadcbe850a72ecb23ba
Karthick Raj K <karthickraj2001@gmail.com>
2023-03-22 08:09:32 +0530

    Task Completed

--- src/main/java/com/driver/services/ImageService.java
+++ src/main/java/com/driver/services/ImageService.java
@@ -17,15 +17,28 @@ public class ImageService {
 
     public Image addImage(Integer blogId, String description, String dimensions){
         //add an image to the blog
-
+        Blog blog = blogRepository2.findById(blogId).get();
+        Image image = new Image();
+        image.setDescription(description);
+        image.setDimensions(dimensions);
+        image.setBlog(blog);
+        blog.getImageList().add(image);
+        blogRepository2.save(blog);
+        return image;
     }
 
     public void deleteImage(Integer id){
-
+        imageRepository2.deleteById(id);
     }
 
     public int countImagesInScreen(Integer id, String screenDimensions) {
         //Find the number of images of given dimensions that can fit in a screen having `screenDimensions`
+        Image image = imageRepository2.findById(id).get();
+        String[] screenDimension = screenDimensions.split("X");
+        String[] imageDimension = image.getDimensions().split("X");
+        int len = Integer.valueOf(imageDimension[0])/Integer.valueOf(screenDimension[0]);
+        int bre = Integer.valueOf(imageDimension[1])/Integer.valueOf(screenDimension[1]);
+        return len*bre;
 
     }
 }
